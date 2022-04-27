e9a86e79f79019219a82bb9d7e928abde3ef9033
Kushal Kumar <kushalkumargupta4@gmail.com>
2022-04-27 16:35:22 +0530

    fix: update DB STRING
    Signed-off-by: Kushal Kumar <kushalkumargupta4@gmail.com>

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { errorHandler, NotFoundError, connectDB } = require("prattask-cmmn");
 const userRoutes = require("./routes/user");
 
 const app = express();
-const DB_STRING = "mongodb://mongo-user:27017/users";
+// const DB_STRING = "mongodb://mongo-user:27017/users";
 // const DB_STRING = "mongodb://127.0.0.1:27017/users";
-// const DB_STRING = "mongodb+srv://kushal:pratilipi@users.obikz.mongodb.net/users?retryWrites=true&w=majority";
+const DB_STRING = "mongodb+srv://kushal:pratilipi@users.obikz.mongodb.net/users?retryWrites=true&w=majority";
 
 connectDB(DB_STRING);
 
